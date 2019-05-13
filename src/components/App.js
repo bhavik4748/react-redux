604935c463a0330c6e07b5ebf42fc789f7734a67
Bhavik Gosrani <bhavik4748@gmail.com>
2019-05-13 18:39:37 -0400

    Added PageNofFound component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
 
 function App() {
 
     return (
         <div className="container-fluid">
-            <Route exact path="/" component={HomePage}></Route>
-            <Route path="/about" component={AboutPage}></Route>
+            <Header></Header>
+            <Switch>
+                <Route exact path="/" component={HomePage}></Route>
+                <Route path="/about" component={AboutPage}></Route>
+                <Route component={PageNotFound}></Route>
+            </Switch>
         </div>
     );
 }
